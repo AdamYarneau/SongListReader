d8f042a7c6c175aae4889f0f3a66036d15ed8d58
AdamYarneau <86415795+AdamYarneau@users.noreply.github.com>
2022-01-24 13:35:10 -0600

    Finished (?) versions of scripts

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
 import numpy as np
-from PIL import ImageGrab, Image
-import cv2
+from PIL import ImageGrab
 import pytesseract
 import time
 from datetime import datetime
 
 def main():
-    runLength = time.time() + 10
-    now = datetime.now()
+    # Generate Text file name
     startString = "C:/Users/yarne/Desktop/RunLogs/"
-    midString = now.strftime("%H-%M-%S")
-    endString = ".txt"
-    startString += midString
-    startString += endString
+    startString += datetime.now().strftime("%H-%M-%S")
+    startString += ".txt"
+
+    # Create and open file
     fileObj = open(startString,"w")
 
+    runLength = time.time() + 5
     while time.time() < runLength:
+        
+        # Set the boundaries to capture images
         leftBorder = 1950
         topBorder = 396
         rightBorder = 3300
         height = 70
 
-        preImg = ImageGrab.grab(bbox=(leftBorder, topBorder, rightBorder, topBorder + height),all_screens=False)#.convert("L")
-        
+        preImg = ImageGrab.grab(bbox=(leftBorder, topBorder, rightBorder, topBorder + height),all_screens=False)
         img = np.array(preImg)
-        cv2.imshow('window', img)
         txt = pytesseract.image_to_string(img)
-
         print(txt)      
         fileObj.write(txt)
-
-        if cv2.waitKey(25) & 0xFF == ord('q'):  
-            cv2.destroyAllWindows()
-            break
 
     fileObj.close()
 
